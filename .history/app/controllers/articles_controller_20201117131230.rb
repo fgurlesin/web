c98b114f2dd6955fb4c185d8aec11beec27e7d71
chopinhauer <farukgurlesin@hotmail.com>
2020-11-17 14:04:05 +0300

    Categories , Articles style v.1.2

---
+++ .history/app/controllers/articles_controller_20201117131230.rb
@@ -0,0 +1,74 @@
+class ArticlesController < ApplicationController
+
+  before_action :set_article , only: [ :show , :edit , :update , :destroy]
+  before_action :require_user , except: [:show , :index]
+  before_action :require_same_user , only: [:edit , :update ,:destroy ]
+  
+
+  def show
+    @category=Category.find(params[:id])
+    @article=@category.articles
+  end
+
+  def index 
+    @article = Article.all.order("updated_at DESC").paginate(page: params[:page], per_page: 5)
+    @category=Category.all
+  end
+
+  def new
+    @article=Article.new
+  end
+  
+  def create 
+    @article = Article.new(article_params)
+    @article.user = current_user
+    if @article.save
+      flash[:notice] = "Article was created succesfully"
+      #redirect_to articles_path
+      redirect_to @article
+    else
+      render 'new'
+    end
+    
+  end
+
+  def edit
+    
+  end
+
+  def update 
+    if @article.update(article_params)
+      flash[:notice]="Article was updated succesfully"
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path 
+  end
+
+  private
+
+  def set_article 
+  @article= Article.find(params[:id])
+  
+
+  end
+
+  def article_params
+    params.require(:article).permit(:tittle, :description , :image , category_ids: [] )
+  end
+
+  def require_same_user
+    if current_user != @article.user && !current_user.admin?
+      flash[:alert] =  "You can only edit or delete your article"
+    
+    end 
+  end
+
+  public
+
+end
---
+++ .history/app/models/article_20201117095648.rb
@@ -0,0 +1,10 @@
+class Article < ApplicationRecord
+
+  belongs_to :user 
+  has_many :article_categories
+  has_many :categories , through: :article_categories
+  validates :tittle       , presence: true , length: {minimum:5 }
+  validates :description  , presence: true , length: {minimum:10 }
+  has_many :comments
+  has_one_attached :image 
+end
